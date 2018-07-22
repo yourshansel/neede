3a1aa43403645535033c0ede6b8f56a9d4cdbc32
yourshansel <chenhan.sg@gmail.com>
2018-07-22 15:02:09 -0400

    Added Card Component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import CardComponent from './CardComponent.js';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div>
+        <p className="headertext">
+          Testing Code.
         </p>
+        <CardComponent/>
       </div>
     );
   }
---
+++ src/CardComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class CardComponent extends Component {
+  render() {
+    return (
+      <p>Test Component</p>
+    );
+  }
+}
+
+export default CardComponent;
