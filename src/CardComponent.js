b70b44d968295f06644cf2df2747f7d73e0704dc
yourshansel <chenhan.sg@gmail.com>
2018-07-23 21:45:17 -0400

    Added dataset

--- src/CardComponent.js
+++ src/CardComponent.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+import { NavLink, Card, Button, CardTitle, CardImg, CardBody, CardBlock, CardText, Row, Col } from 'reactstrap';
 
 class CardComponent extends Component {
-  render() {
-    return (
-      <p>Test Component</p>
-    );
+  constructor(props) {
+    super(props);
   }
-}
-
+  render() {
+    let {category, name, description, url } = this.props.cards;
+    return (
+      <div>
+        <Card>
+          <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
+          <CardBlock>
+            <NavLink href={url} target="_blank">{name}</NavLink>
+            <CardText>{description}</CardText>
+            <CardText>{category}</CardText>
+          </CardBlock>
+        </Card>
+      </div>
+      );
+    };
+  };
 export default CardComponent;
