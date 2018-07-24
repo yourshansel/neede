096046c0cd831788521715fe8465d7147f0c0c11
yourshansel <chenhan.sg@gmail.com>
2018-07-24 02:46:13 -0400

    Updated Dataset

--- src/CardComponent.js
+++ src/CardComponent.js
@@ -1,22 +1,17 @@
 import React, { Component } from 'react';
-import { NavLink, Card, Button, CardTitle, CardImg, CardBody, CardBlock, CardText, Row, Col } from 'reactstrap';
+import './CardComponent.css';
 
 class CardComponent extends Component {
   constructor(props) {
     super(props);
   }
   render() {
-    let {category, name, description, url } = this.props.cards;
+    let {category, name, description, url , screenshot} = this.props.data;
     return (
-      <div>
-        <Card>
-          <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-          <CardBlock>
-            <NavLink href={url} target="_blank">{name}</NavLink>
-            <CardText>{description}</CardText>
-            <CardText>{category}</CardText>
-          </CardBlock>
-        </Card>
+      <div className="cardComponent">
+      <img  src={screenshot} alt="Card image cap" className="cardComponent_img"/>
+      <a href={url} className="cardComponent_name">{name}</a>
+      <p className="cardComponent_description">{description}</p>
       </div>
       );
     };
