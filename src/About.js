35ec1f45d720f1245c214a85c77ffd39ab3dce44
yourshansel <chenhan.sg@gmail.com>
2018-09-04 17:31:48 +0800

    Update email

--- src/About.js
+++ src/About.js
@@ -25,7 +25,7 @@ class About extends Component {
       <div className="about-header">How does the favorites work?</div>
       <div className="about-description">When you click on the favourite button, the website is being saved on your browser’s local storage so you can manage and access them on the favourites tab.</div>
       <div className="about-header">Found something awesome that you want to share?</div>
-      <div className="about-description">Send me any links or categories you’d like to see added to Neede at <a href="mailto:neede.design@gmail.com" className="about-link">neede.design@gmail.com</a>.</div>
+      <div className="about-description">Send me any links or categories you’d like to see added to Neede at <a href="mailto:yourshansel@gmail.com" className="about-link">yourshansel@gmail.com</a>.</div>
       <div className="about-description"><br/>Neede was founded and curated by <a href="https://twitter.com/yourshansel" className="about-link">Hansel Wong</a></div>
       <div className="about-spacer"/>
       </div>
