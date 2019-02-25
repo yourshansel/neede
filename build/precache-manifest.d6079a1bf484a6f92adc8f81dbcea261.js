812465c60a52b628d593ea2a0ce47f85d8057794
yourshansel <chenhan.sg@gmail.com>
2019-02-26 00:40:52 +0800

    update library

---
+++ build/precache-manifest.d6079a1bf484a6f92adc8f81dbcea261.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "2695b7a777fd7b3d821f",
+    "url": "/static/js/main.2695b7a7.chunk.js"
+  },
+  {
+    "revision": "9875edad711666277af5",
+    "url": "/static/js/2.9875edad.chunk.js"
+  },
+  {
+    "revision": "2695b7a777fd7b3d821f",
+    "url": "/static/css/main.0b871e0c.chunk.css"
+  },
+  {
+    "revision": "ddbbc600dba12c715a00d0e8b62110ed",
+    "url": "/index.html"
+  }
+];
