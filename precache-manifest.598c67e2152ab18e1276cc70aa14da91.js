ed3f147404ad53548497afd9346bdbf9b3ab5fc7
yourshansel <chenhan.sg@gmail.com>
2019-03-01 13:19:42 +0800

    Updates

---
+++ precache-manifest.598c67e2152ab18e1276cc70aa14da91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "61e7163602baa5ebe07f",
+    "url": "/static/js/main.61e71636.chunk.js"
+  },
+  {
+    "revision": "9875edad711666277af5",
+    "url": "/static/js/2.9875edad.chunk.js"
+  },
+  {
+    "revision": "61e7163602baa5ebe07f",
+    "url": "/static/css/main.0b871e0c.chunk.css"
+  },
+  {
+    "revision": "d30dfb6648fe1653490392a3780c9886",
+    "url": "/index.html"
+  }
+];
