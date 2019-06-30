36f05037792ecef983d83bd65f4a32fe5f448315
yourshansel <chenhan.sg@gmail.com>
2019-06-30 19:25:59 +0800

    Updates

---
+++ precache-manifest.df4429fa3f431e510ee7bd0c8e8ad139.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "f59bcbc475e9757e7fb4",
+    "url": "/static/js/main.f59bcbc4.chunk.js"
+  },
+  {
+    "revision": "b52b6a31a3742ca2b73a",
+    "url": "/static/js/2.b52b6a31.chunk.js"
+  },
+  {
+    "revision": "f59bcbc475e9757e7fb4",
+    "url": "/static/css/main.2f5d72c9.chunk.css"
+  },
+  {
+    "revision": "6ef254c2eb743d5c73a1c302fa96b82f",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.ac4675a67754d6ccadccbe11186a1e9e.js"
+  "/precache-manifest.df4429fa3f431e510ee7bd0c8e8ad139.js"
 );
 
 workbox.clientsClaim();
