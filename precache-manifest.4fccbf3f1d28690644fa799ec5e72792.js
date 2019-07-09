8096e708beb51eb2a9fde3d749552c258c7206ca
yourshansel <chenhan.sg@gmail.com>
2019-07-09 22:34:26 +0800

    Updates

--- precache-manifest.4fccbf3f1d28690644fa799ec5e72792.js
+++ precache-manifest.4fccbf3f1d28690644fa799ec5e72792.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.42ac5946.js"
   },
   {
-    "revision": "ef567af729cfad325d85",
-    "url": "/static/js/main.ef567af7.chunk.js"
+    "revision": "d1b0a311f53e9d9f3c7f",
+    "url": "/static/js/main.d1b0a311.chunk.js"
   },
   {
     "revision": "b52b6a31a3742ca2b73a",
     "url": "/static/js/2.b52b6a31.chunk.js"
   },
   {
-    "revision": "ef567af729cfad325d85",
-    "url": "/static/css/main.c3659a88.chunk.css"
+    "revision": "d1b0a311f53e9d9f3c7f",
+    "url": "/static/css/main.64f9d85d.chunk.css"
   },
   {
-    "revision": "b6f023dbe20543cb77c733e3975e0465",
+    "revision": "a8b82a4570692b57c19a7101e2317969",
     "url": "/index.html"
   }
 ];
