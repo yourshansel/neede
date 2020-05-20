caffc3800b1dcd633218fb561aa5b89d8e1ef8e8
Hansel Wong <hanselwong@Hansels-iMac.local>
2020-05-20 21:21:11 +0800

    Updates

---
+++ precache-manifest.550058b2505230d149e5897e5a21c1b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112fdf3f86ef0352b073315847f25558",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f70fc83069aec378d8c",
+    "url": "/static/css/main.df68a096.chunk.css"
+  },
+  {
+    "revision": "fa23d3198ae6e9c400e6",
+    "url": "/static/js/2.312fc13c.chunk.js"
+  },
+  {
+    "revision": "0f70fc83069aec378d8c",
+    "url": "/static/js/main.ba2b4a6d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
